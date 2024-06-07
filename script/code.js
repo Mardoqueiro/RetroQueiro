728c7928c3d2f47552357000947710ab00e37b3d
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-06-07 16:29:25 +0200

    currentYear

--- script/code.js
+++ script/code.js
@@ -48,6 +48,34 @@ let products =
         )
     )
 
+    // current year
+let currentYear = new Date().getUTCFullYear()
+document.querySelector("[currentYear]").textContent = currentYear
+function recentProducts() {
+    try{
+        let arrSize = products.length
+        let latestProducts = products.reverse().slice(0, arrSize >> 1)
+            latestProducts.forEach(product => {
+            wrapper.innerHTML += `
+            <div class="card">
+                <img src="${product.image}" class="card-img-top" alt="${product.productName}" loading="lazy">
+                <div class="card-body">
+                  <h5 class="card-title">${product.productName}</h5>
+                  <p class="card-text">${product.description}</p>
+                </div>
+            </div>
+            `
+        })
+    } catch (e) {
+        wrapper.textContent = "Please contact our administrator"
+        seetTimeout(() => {
+            location.reload()  
+        },
+            2000  
+        )
+    }
+}
+
 function recentProducts() {
     try{
         let arrSize = products.length
